1fd4f550cfe722483a7a05747c7b0bafba965d3f
vvyvi <ewawozny4@gmail.com>
2021-04-15 21:11:02 +0200

    blocker fix

--- src/hooks/PlayerCards.tsx
+++ src/hooks/PlayerCards.tsx
@@ -74,8 +74,7 @@ const PlayerCards = (props: { children: boolean | React.ReactChild | React.React
                     })
                 break;
         }
-        //console.log('white', tmpWhite)
-        //setCanCommit(false)
+
         for (let i = 0; i < tmpWhite.length; i++) {
             if (tmpWhite[i].card === undefined) {
                 tmpWhite.splice(i, 1)
@@ -97,37 +96,6 @@ const PlayerCards = (props: { children: boolean | React.ReactChild | React.React
     useEffect(() => {
         socket.on('recieveWhite', function (id: number, card: Card, cardSauce: CardSauce) {
             setTmp({card: card, sauce: cardSauce})
-
-            /*if(cardSauce.type===2) {
-              let div1 = document.createElement("div");
-              div1.className="info_chat_input";
-
-              /*<div className="info_chat_input">
-                <input id="customInput" placeholder="Tu wpisz tekst customowej karty" aria-label="Tu wpisz tekst customowej karty" />
-                <button type="button" onClick={writeCustom}>Send</button>
-              </div>*/
-
-            /*let inp1 = document.createElement("input");
-            inp1.id = "customInput";
-            inp1.setAttribute("placeholder", "Tu wpisz tekst customowej karty");
-            inp1.setAttribute("aria-label", "Tu wpisz tekst customowej karty");
-
-            let but1 = document.createElement("button");
-            but1.setAttribute("type", "button");
-            but1.innerHTML = "Send";
-            but1.onclick = () => writeCustom();
-
-            div1.appendChild(inp1);
-            div1.appendChild(but1);
-
-            displayCustom(div1);
-          }
-
-          displayMessage({
-            date: '',
-            author: "white card",
-            sauce: "["+card.matchid+"] "+cardSauce.text
-          });*/
         })
 
         socket.on('loadloader', function (number: string) {
